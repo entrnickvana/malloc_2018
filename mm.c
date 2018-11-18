fbee0dddb6c7e81ad52dfa5f12628079ffc6f364
nick elliott <Nick.jwl123@gmail.com>
2018-11-18 13:38:45 -0700

    working --single

--- mm.c
+++ mm.c
@@ -25,6 +25,8 @@ void *coalesce(void *bp);
 int max(int a, int b);
 void p_debug(char* str);
 void set_allocated1(void *bp, size_t size);
+void p_addr(void* begin, void* end, void* current);
+int dbg = 0;
 
 typedef struct {
   size_t size;
@@ -44,6 +46,8 @@ typedef struct list_node {
 
 void* first_bp;
 void* free_list;
+void* heap_gbl;
+void* end_heap_gbl;
 
 int init_count = 0;
 int mm_malloc_count = 0;
@@ -56,24 +60,21 @@ int coal_count = 0;
 void mm_init(void *heap, size_t heap_size)
 {
   p_debug("mm_init");
-  printf("HEAP SIZE: %zu\n", heap_size);
+  if(dbg) printf("HEAP SIZE: %zu\n", heap_size);
+  heap_gbl = heap;
+
   init_count++;
   void *bp;
-  if(!IS_ALIGNED(heap)) printf("heap not aligned: %i\n", IS_ALIGNED(heap));
-  else printf("heap aligned: %i\n", IS_ALIGNED(heap));
   
   bp = (void*)heap + sizeof(block_header);
-
-  if(!IS_ALIGNED(bp)) printf("bp not aligned: %i\n", IS_ALIGNED(bp));
-  else printf("bp aligned: %i\n", IS_ALIGNED(bp));
-
 
   GET_SIZE(HDRP(bp)) = 2;   // Install prolog HDR
   GET_ALLOC(HDRP(bp)) = 1;  
   GET_SIZE(bp) = 2;   // Install prolog FTR
 
   void* terminator = (void*)heap + heap_size; // Install terminator block
-
+  if(dbg) printf("TERMINATOR ADDR: %p\n" , terminator);
+  end_heap_gbl = terminator;
   GET_SIZE(HDRP(terminator)) = 0;
   GET_ALLOC(HDRP(terminator)) = 1;
 
@@ -93,9 +94,12 @@ void set_allocated1(void *bp, size_t size) {
   p_debug("set_allocated");    
   set_allocated_count++;    
   size_t extra_size = GET_SIZE(HDRP(bp)) - size;
-  printf("EXTRA SIZE: %zu\n", extra_size);
+  if(dbg) printf("EXTRA SIZE: %zu\n", extra_size);
+  if(dbg) printf("Align(1+overhead) = %li\n", ALIGN(1 + OVERHEAD));
   if (extra_size > ALIGN(1 + OVERHEAD)) {
+    if(dbg) printf("\n********\nSPLITTING \n\n");
     GET_SIZE(HDRP(bp)) = size;
+    if(dbg) printf("FIRST HALF SIZE: %zu\t\tSECOND HALF SIZE: %zu\n", size, extra_size);
     GET_SIZE(HDRP(NEXT_BLKP(bp))) = extra_size;
     GET_ALLOC(HDRP(NEXT_BLKP(bp))) = 0;
   }
@@ -117,21 +121,29 @@ void *mm_malloc(size_t size)
   int new_size = ALIGN(need_size + OVERHEAD);
   void *bp = first_bp;
   int loop_count = 0;
+  if(dbg) printf("size list node %li\n", sizeof(list_node));
+  if(dbg) printf("REQUESTED SIZE: %zu\t\t NEED SIZE: %i\t\tNEW SIZE: %i\n", size, need_size, new_size);
 
   while (GET_SIZE(HDRP(bp)) != 0) {
-    printf("loop: %i\t SIZE: %zu\n", loop_count++, GET_SIZE(HDRP(bp)));
+    if(dbg) printf("---------------------loop: %i ---------------\n SIZE: %zu\n", loop_count++, GET_SIZE(HDRP(bp)));
+    if(dbg) printf("DIST FROM HEAP: %li\n", bp-heap_gbl);
     if (!GET_ALLOC(HDRP(bp)) && (GET_SIZE(HDRP(bp)) >= new_size)) {
       set_allocated1(bp, new_size);
       set_allocated(bp);
 
-      if(!IS_ALIGNED(bp)) printf("bp not aligned: %i\n", IS_ALIGNED(bp));
-      else printf("bp aligned: %i\n", IS_ALIGNED(bp));
+      if(!IS_ALIGNED(bp)){
+       if(dbg) {printf("bp not aligned: %i\n", IS_ALIGNED(bp));}
+      }
+      else if(dbg) printf("bp aligned: %i\n", IS_ALIGNED(bp));
 
-
+      if(dbg) printf("-----------------END MALLOC-----------------\n\n\n");
       return bp;
     }
+
     bp = NEXT_BLKP(bp);
   }
+
+  if(dbg) printf("FAIL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
 
   return NULL;
 }
@@ -141,8 +153,9 @@ void mm_free(void *bp)
 {
   p_debug("mm_free");  
   free_count++;  
-  GET_ALLOC(HDRP(bp)) = 0;
   coalesce(bp);
+  GET_ALLOC(HDRP(bp)) = 0;
+
 }
 
 void *coalesce(void *bp) {
@@ -151,43 +164,60 @@ void *coalesce(void *bp) {
   size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(bp)));
   size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
   size_t size = GET_SIZE(HDRP(bp));
+  if(dbg) printf("COAL SIZE: %zu\t PREV_ALLOC: %zu\t next_alloc %zu\n", size, prev_alloc, next_alloc);
+
+  if(dbg) printf("prev_aloc completed\n");
   
     if (prev_alloc && next_alloc) { /* Case 1 */
       //add_to_free_list((list_node *)bp);
+      if(dbg) printf("case 1\n");
     }
     else if (prev_alloc && !next_alloc) { /* Case 2 */
-     size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-     GET_SIZE(HDRP(bp)) = size;
-     GET_SIZE(FTRP(bp)) = size;
+      if(dbg) printf("case 2\n");    
+      size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+      GET_SIZE(HDRP(bp)) = size;
+      GET_SIZE(FTRP(bp)) = size;
     }
     else if (!prev_alloc && next_alloc) { /* Case 3 */
-     size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-     GET_SIZE(FTRP(bp)) = size;
-     GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
-     bp = PREV_BLKP(bp);
+      if(dbg) printf("case 3\n");    
+      size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+      GET_SIZE(FTRP(bp)) = size;
+      GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
+      bp = PREV_BLKP(bp);
     }
     else { /* Case 4 */
-     size += (GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp))));
-     GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
-     GET_SIZE(FTRP(NEXT_BLKP(bp))) = size;
-     bp = PREV_BLKP(bp);
+      size += (GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp))));
+      if(dbg) printf("CASE 4 SIZE: %zu\n ", size);     
+      GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
+      if(dbg) printf("ATTEMPT\n");
+      if(dbg) printf("CURR BLK SIZE: %zu\n", GET_SIZE(HDRP(bp)));
+      //if(dbg) printf("NXT BLK SIZE: %zu\n", GET_SIZE(HDRP(NEXT_BLKP(bp))));
+      p_addr(heap_gbl, end_heap_gbl, NEXT_BLKP(bp));
+      GET_SIZE(FTRP(NEXT_BLKP(bp))) = size;  // seg fault
+
+      bp = PREV_BLKP(bp);
     }
-  
+
   return bp;
 
 }
 
 void p_debug(char* str)
 {
-  printf("init: %i\tset_alloc: %i\tmm_alloc: %i\tfree: %i\tcoal: %i\n", init_count,set_allocated_count, mm_malloc_count, free_count, coal_count);
-  printf(str);
-  printf("\n");
+  if(dbg) printf("init: %i\tset_alloc: %i\tmm_alloc: %i\tfree: %i\tcoal: %i\n", init_count,set_allocated_count, mm_malloc_count, free_count, coal_count);
+  if(dbg) printf(str);
+  if(dbg) printf("\n");
 }
 
 int max(int a, int b)
 {
   if(a >= b) return a;
   return b;
+}
+
+void p_addr(void* begin, void* end, void* current)
+{
+  if(dbg) printf("BEGIN:\t\t%p\t\tEND:\t\t%p\t\tCURRENT\t\t%p\n", begin, end, current);
 }
 
 
